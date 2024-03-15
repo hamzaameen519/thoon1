10011f6e75bcf0522492989f46c3753138512ea1
Zohaib Shahid <zohaib.shahid@ebryx.com>
2024-03-16 02:19:45 +0500

    feat: card add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import GameZone from "./components/GameZone/GameZone";
+import Card from "./components/Card/Card";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="sc-eDLKkx eTbKOW">
-      <Navbar />
-      <GameZone />
+    <div className='sc-eDLKkx eTbKOW !px-40'>
+      {/* <Navbar />
+      <GameZone /> */}
+      <div className='flex justify-between flex-wrap'>
+        <div className='mb-16'>
+          <Card />
+        </div>
+        <div className='mb-16'>
+          <Card />
+        </div>
+        <div className='mb-16'>
+          <Card />
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
