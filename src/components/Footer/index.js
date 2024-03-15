c12d06f76cf0d67c8e5f5169c42e185db59f40a7
Zohaib Shahid <zohaib.shahid@ebryx.com>
2024-03-16 03:10:06 +0500

    feat: card add title

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
       </div>
       <div className="border-2 py-6 px-4 bg-white text-black flex justify-between items-center lg:items-end pb-14 rounded-lg w-full mb-8 flex-col lg:flex-row">
         <div className="flex flex-col mb-4 lg:mb-0">
-          <h3 className="text-3xl font-medium mb-1">{0.0} USDT</h3>
+          <h3 className="text-3xl font-medium mb-1">0.0000 USDT</h3>
           <span className="font-semibold">Total Reward</span>
         </div>
         <button className="p-3 bg-[#4c1f14] text-white rounded-md w-80">Claim Reward</button>
