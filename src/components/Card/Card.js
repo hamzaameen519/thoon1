5bb294ba696742d61bcb3c6f8dbcb6499c3897c7
Zohaib Shahid <z-shaid@ejada.com>
2024-03-16 01:36:43 +0500

    feat: changes for card and footer

---
+++ src/components/Card/Card.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./card.css";
+
+export default function Card() {
+  return (
+    <div className='w-[22rem] bg-white text-black rounded-lg border-2 relative'>
+      <div className='ribbon ribbon-top-left'>
+        <span>LIVE</span>
+      </div>
+
+      <div className='p-6 font-medium'>
+        <div className='w-32 border-[] p-2 border-2 rounded-full overflow-hidden mb-5'>
+          <img className='w-full' src='https://i.pinimg.com/736x/9a/64/8a/9a648ac91d323b0931448ab34055f967.jpg' alt='' />
+        </div>
+        <div className='flex justify-between mb-4'>
+          <span>APY</span>
+          <span>{600}%</span>
+        </div>
+        <div className='flex justify-between'>
+          <span>Lock Duration</span>
+          <span>{60} Days</span>
+        </div>
+
+        <div className='flex items-center flex-col mt-10 mb-8 space-y-1'>
+          <span>Your Wallet</span>
+          <span>{0} $BABYGECKO</span>
+        </div>
+
+        <div className='flex flex-col space-y-5'>
+          <input className='bg-[#242323] shadow-lg shadow-[#383838] rounded-md py-2 px-4 text-gray-400 text-lg font-normal placeholder-gray-500' placeholder='Enter BABYGECKO to Stake' />
+          <button className='p-3 bg-[#4c1f14] text-white rounded-md'>Stake</button>
+        </div>
+      </div>
+      <hr />
+      <p className='text-xs font-medium text-center pb-4'>Stake $BABYGECKO Token and Earn 730% APY</p>
+    </div>
+  );
+}
---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className='flex flex-col xl:flex-row w-full xl:space-x-8'>
+      <div className='border-2 py-6 px-4 bg-white text-black flex justify-between items-center lg:items-end pb-14 rounded-lg w-full mb-8'>
+        <div className='flex flex-col'>
+          <h3 className="text-3xl font-medium mb-1">{0} BABYGECKO</h3>
+          <span className="font-semibold">Your total Staked Token</span>
+        </div>
+      </div>
+      <div className='border-2 py-6 px-4 bg-white text-black flex justify-between items-center lg:items-end pb-14 rounded-lg w-full mb-8 flex-col lg:flex-row'>
+        <div className='flex flex-col mb-4 lg:mb-0'>
+          <h3 className="text-3xl font-medium mb-1">{0} BABYGECKO</h3>
+          <span className="font-semibold">Your total Staked Token</span>
+        </div>
+        <button className='p-3 bg-[#4c1f14] text-white rounded-md w-80'>Claim Reward</button>
+      </div>
+    </div>
+  );
+}
