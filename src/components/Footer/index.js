0a94aecfd91bbc810c3246d62963096f56a1b46c
Zohaib Shahid <z-shaid@ejada.com>
2024-03-16 01:38:56 +0500

    feat; chage text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,8 +11,8 @@ export default function Footer() {
       </div>
       <div className='border-2 py-6 px-4 bg-white text-black flex justify-between items-center lg:items-end pb-14 rounded-lg w-full mb-8 flex-col lg:flex-row'>
         <div className='flex flex-col mb-4 lg:mb-0'>
-          <h3 className="text-3xl font-medium mb-1">{0} BABYGECKO</h3>
-          <span className="font-semibold">Your total Staked Token</span>
+          <h3 className="text-3xl font-medium mb-1">{0.00000} USDT</h3>
+          <span className="font-semibold">Total Reward</span>
         </div>
         <button className='p-3 bg-[#4c1f14] text-white rounded-md w-80'>Claim Reward</button>
       </div>
